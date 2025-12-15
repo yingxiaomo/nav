576cbc01e0508820a726fa0fef867d061c7b24e9
YingXiaoMo <yingxiaomoya@gmail.com>
2025-12-16 00:06:05 +0800

    123

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,19 @@
 import type { NextConfig } from "next";
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", 
+});
 
 const nextConfig: NextConfig = {
   output: process.env.DOCKER_BUILD === "true" 
     ? "standalone" 
     : (process.env.VERCEL ? undefined : "export"),
-    
   compress: true,
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
